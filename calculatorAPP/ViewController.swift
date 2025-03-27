ede7ae2121e315ddc21618593975112567d0d470
yeungrak <tifshadk@gmail.com>
2025-03-27 20:16:26 +0900

    Start Lv.1까지 구현

--- calculatorAPP/ViewController.swift
+++ calculatorAPP/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private let lable = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configure()
     }
-
-
+    
+    private func configure() {
+        view.backgroundColor = .black
+        
+        lable.text = "12345"
+        lable.textColor = .white
+        lable.textAlignment = .right
+        lable.font = UIFont.boldSystemFont(ofSize: 60)
+        
+//        [lable]
+//            .forEach { view.addSubview($0) }
+        view.addSubview(lable)
+        lable.snp.makeConstraints {
+            $0.leading.equalTo(view.snp.leading).offset(30)
+            $0.trailing.equalTo(view.snp.trailing).offset(-30)
+            $0.top.equalTo(view.snp.top).offset(200)
+            $0.height.equalTo(100)
+            $0.width.equalTo(10)
+        }
+        
+    }
 }
 
