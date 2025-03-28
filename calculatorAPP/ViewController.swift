b6c7a5317b79e0b3c4239a6a39286abfe1f87364
yeungrak <tifshadk@gmail.com>
2025-03-28 12:05:26 +0900

    feat: 4개의 HoizontalStackView 생성

--- calculatorAPP/ViewController.swift
+++ calculatorAPP/ViewController.swift
@@ -10,30 +10,32 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    
+    //계산기의 레이블, 버튼등 구성요소 만들기
     private let label = UILabel()
-    
     private let stackView = UIStackView()
-    
-    private let numberButton: [UIButton] = (1...9).map { num in
+    private let plusButton = UIButton()
+    private let minusButton = UIButton()
+    private let divisionButton = UIButton()
+    private let multiplyButton = UIButton()
+    private let resetButton = UIButton()
+    private let resultButton = UIButton()
+    private let numberButton: [UIButton] = (0...9).map { num in
         let button = UIButton()
         button.setTitle("\(num)", for: .normal)
         button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
         button.layer.cornerRadius = 40
+        button.titleLabel?.font = .boldSystemFont(ofSize: 30)
         button.snp.makeConstraints { $0.size.equalTo(80) }
         return button
     }
-    
-    private let plusButton = UIButton()
-    
-    private let minusButton = UIButton()
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
     
+    //스택뷰를 만드는 함수 만들기
     private func makeHoizontalStackView(views: [UIView]) -> UIStackView {
         let stackView = UIStackView(arrangedSubviews: views)
         //uiView를 써야함
@@ -43,36 +45,74 @@ class ViewController: UIViewController {
         stackView.distribution = .fillEqually
         return stackView
     }
-
+    
     
     
     private func configure() {
         view.backgroundColor = .black
         
-        //stackView
-        let stackView = makeHoizontalStackView(views: [numberButton[6], numberButton[7], numberButton[8], plusButton])
         
-        //lable
+        //stackView를 만드는 함수를 이용해 1~4까지의 스택뷰 만들기
+        let stackView = makeHoizontalStackView(views: [numberButton[7], numberButton[8], numberButton[9], plusButton])
+        let stackView2 = makeHoizontalStackView(views: [numberButton[4], numberButton[5], numberButton[6], minusButton])
+        let stackView3 = makeHoizontalStackView(views: [numberButton[1], numberButton[2], numberButton[3], multiplyButton])
+        let stackView4 = makeHoizontalStackView(views: [resetButton, numberButton[0], resultButton, divisionButton])
+        
+        
+        
+        //lable 구성요소
         label.text = "12345"
         label.textColor = .white
         label.textAlignment = .right
         label.font = UIFont.boldSystemFont(ofSize: 60)
         
-        //plusbutton
+        //plusbutton 구성요소
         plusButton.setTitle("+", for: .normal)
         plusButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
         plusButton.layer.cornerRadius = 40
+        plusButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
         plusButton.snp.makeConstraints { $0.size.equalTo(80) }
         
-        //minusbutton
+        //minusbutton 구성요소
         minusButton.setTitle("-", for: .normal)
+        minusButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        minusButton.layer.cornerRadius = 40
+        minusButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        minusButton.snp.makeConstraints { $0.size.equalTo(80) }
+        
+        //divisionButton 구성요소
+        divisionButton.setTitle("/", for: .normal)
+        divisionButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        divisionButton.layer.cornerRadius = 40
+        divisionButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        divisionButton.snp.makeConstraints { $0.size.equalTo(80) }
+        
+        //multiplyButton 구성요소
+        multiplyButton.setTitle("*", for: .normal)
+        multiplyButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        multiplyButton.layer.cornerRadius = 40
+        multiplyButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        multiplyButton.snp.makeConstraints { $0.size.equalTo(80) }
+        
+        //resetButton 구성요소
+        resetButton.setTitle("AC", for: .normal)
+        resetButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        resetButton.layer.cornerRadius = 40
+        resetButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        resetButton.snp.makeConstraints { $0.size.equalTo(80) }
+        
+        //resultButton 구성요소
+        resultButton.setTitle("=", for: .normal)
+        resultButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        resultButton.layer.cornerRadius = 40
+        resultButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        resultButton.snp.makeConstraints { $0.size.equalTo(80) }
         
         
+        //forEach로 한번에 뷰에 추가하기
+        [label, stackView, stackView2, stackView3, stackView4]
+            .forEach { view.addSubview($0) }
         
-        
-        
-        view.addSubview(label)
-        view.addSubview(stackView)
         
         
         //label Constraints
@@ -83,18 +123,27 @@ class ViewController: UIViewController {
             $0.height.equalTo(100)
             $0.width.equalTo(10)
         }
-            
+        
         //stackView Constraints
         stackView.snp.makeConstraints{
-                $0.height.equalTo(80)
-                $0.center.equalToSuperview()
-            }
-            
+            $0.height.equalTo(80)
+            $0.center.equalToSuperview()
         }
         
-        
+        stackView2.snp.makeConstraints{
+            $0.height.equalTo(80)
+            $0.center.equalToSuperview()
+        }
+        stackView3.snp.makeConstraints{
+            $0.height.equalTo(80)
+            $0.center.equalToSuperview()
+        }
+        stackView4.snp.makeConstraints{
+            $0.height.equalTo(80)
+            $0.center.equalToSuperview()
+        }
     }
-
-// 일단 버튼마다 상수를 만들고, 거기에 버튼의 설정을 전부다 할당해준다.
-//
+    
+    
+}
 
